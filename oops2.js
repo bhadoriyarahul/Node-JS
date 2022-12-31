7702b92061a9dd007dec66a8d2887b3a339096ab
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2022-12-31 20:24:22 +0530

    Add files via upload

---
+++ oops2.js
@@ -0,0 +1,15 @@
+class Add
+{
+    addData()
+    {
+     var a,b,c;
+     a=10;
+     b=20;
+     c=a+b;
+     console.log("a = "+a);
+     console.log("b = "+b);
+     console.log("add = "+c);
+    }
+}
+    var obj = new Add();
+    obj.addData();
