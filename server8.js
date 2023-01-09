1179df819b857c48b394aedc20976a512e016c0d
Rahul Singh Bhadoriya <118618909+bhadoriyarahul@users.noreply.github.com>
2023-01-09 21:02:02 +0530

    Add files via upload

---
+++ server8.js
@@ -0,0 +1,46 @@
+/* url routing + method checking  */
+
+import http from 'http';
+import url from 'url';
+
+http.createServer((req,res)=>{
+ var urlObj=url.parse(req.url,true);
+ //console.log(urlObj);     
+ var url_route="<h2>Click on the link below to redirect</h2><a href='/home'>Home page</a><br/><a href='/about'>About page</a><br/><a href='/contact'>Contact page</a><br/><a href='/service'>Service page</a><br/><a href='/register'>Register page</a><br/><a href='/login'>Login page</a>";
+ var response_msg;
+ if(urlObj.pathname=="/" || urlObj.pathname=="/home")
+    response_msg="<h1>/ or /home url invoked</h1>";
+ else if(urlObj.pathname=="/about")
+    response_msg="<h1>/about url invoked</h1>";
+ else if(urlObj.pathname=="/contact")
+    response_msg="<h1>/contact url invoked</h1>";
+ else if(urlObj.pathname=="/service")
+ {
+    if(req.method=="GET")
+        response_msg="<h1>/service url invoked & method GET</h1>";
+    else if(req.method=="POST")
+        response_msg="<h1>/service url invoked & method POST</h1>";
+    else if(req.method=="PUT")
+        response_msg="<h1>/service url invoked & method PUT</h1>";
+    else if(req.method=="PATCH")
+        response_msg="<h1>/service url invoked & method PATCH</h1>";
+    else if(req.method=="DELETE")
+        response_msg="<h1>/service url invoked & method DELETE</h1>";
+    else
+        response_msg="<h1>/service url invoked & method Anonymous</h1>";                
+ }
+ else if(urlObj.pathname=="/register")
+    response_msg="<h1>/register url invoked</h1>";
+ else if(urlObj.pathname=="/login")
+ {
+    console.log("User Details");
+    console.log("Username :"+urlObj.query.username);
+    console.log("Password :"+urlObj.query.password);        
+    response_msg="<h1>/login url invoked</h1>";   
+ }
+ else
+    response_msg="<h1>Invalid url invoked</h1>";   
+ res.write(response_msg+url_route);
+ res.end();
+}).listen(8081);
+console.log("server invoked at link http://localhost:8081");
